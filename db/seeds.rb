c7cd2db669add8b3fd5fe098bdd48a26da576332
Seidai Inoue <seikundayo3@yahoo.co.jp>
2020-04-18 06:06:28 +0000

    Modified category

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,5 @@
 Category.find_or_create_by(name:"Work")
 Category.find_or_create_by(name:"Hobby")
 Category.find_or_create_by(name:"Life")
+Category.find_or_create_by(name:"Important")
 Category.find_or_create_by(name:"Other")
